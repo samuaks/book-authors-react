56d6377ed5ee73a041ac86ce4c426164f677460c
samuaks <samusoisalo@hotmail.com>
2022-02-15 09:33:29 +0200

    initial commit with apollo client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { render } from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
 import './App.css';
 
 function App() {
+
+  const client = new ApolloClient({
+    uri: 'http://localhost:5000/graphql',
+    cache: new InMemoryCache()
+  });
+
+  client
+    .query({
+      query: gql`
+        query {
+          books {
+            id
+            name
+          }
+        }
+      `
+    })
+    .then(result => console.log(result));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Apollo 🚀</h2>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
 
-ReactDOM.render(
-  <React.StrictMode>
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+});
+
+
+function BookList() {
+  const { loading, error, data } = useQuery(gql`
+    query {
+      books {
+        id
+        name
+        author {
+          name
+        }
+      }
+    }
+  `);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <ul>
+      {data.books.map(book => (
+        <li key={book.id}>{book.name} - {book.author.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+
+function App() {
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <BookList />
+    </div>
+  );
+}
+
+render(
+
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
